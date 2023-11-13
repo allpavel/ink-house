8622b142dc7dfecadabcb79b7a74f01e3e827c6a
allpavel <platprincipal.adv@gmail.com>
2023-11-13 15:07:03 +0300

    add a cta section

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/Navbar";
 export default function Header() {
   return (
     <header className=" md:bg-main-color-200">
-      <div className="mx-auto flex max-w-7xl flex-col justify-between md:flex-row">
+      <div className="mx-auto flex max-w-6xl flex-col justify-between md:flex-row">
         <Logo />
         <Navbar />
       </div>
