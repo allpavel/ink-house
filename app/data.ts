ef8de67c153d1a57671c9afa3387f6f0961fe720
allpavel <platprincipal.adv@gmail.com>
2023-11-16 14:22:44 +0300

    add a remain data

--- app/data.ts
+++ app/data.ts
@@ -4,6 +4,18 @@ import image3 from "@/public/france/03.webp";
 import image4 from "@/public/france/04.webp";
 import image5 from "@/public/france/05.webp";
 import image6 from "@/public/france/06.webp";
+import image7 from "@/public/germany/01.webp";
+import image8 from "@/public/germany/02.webp";
+import image9 from "@/public/germany/03.webp";
+import image10 from "@/public/germany/04.webp";
+import image11 from "@/public/germany/05.webp";
+import image12 from "@/public/germany/06.webp";
+import image13 from "@/public/england/01.webp";
+import image14 from "@/public/england/02.webp";
+import image15 from "@/public/england/03.webp";
+import image16 from "@/public/england/04.webp";
+import image17 from "@/public/england/05.webp";
+import image18 from "@/public/england/06.webp";
 
 export const franceCards = [
   {
@@ -59,5 +71,119 @@ export const franceCards = [
     title: "Пейзаж с рыбой",
     description: "Цветная литография (40х60)",
     price: "20 000 руб",
+  },
+];
+
+export const germanyCards = [
+  {
+    id: 1,
+    image: image7,
+    alt: "Женщина в зелёном пальто и шляпе",
+    author: "Курт Вернер",
+    title: "Над городом",
+    description: "Цветная литография (40х60)",
+    price: "16 000 руб",
+  },
+  {
+    id: 2,
+    image: image8,
+    alt: "Взрослая птица и птенца гуляют по зелёному лугу",
+    author: "Макс Рихтер",
+    title: "Птенцы",
+    description: "Холст, масло (50х80)",
+    price: "14 500 руб",
+  },
+  {
+    id: 3,
+    image: image9,
+    alt: "Высокая птица с белой головой и коричневыми перьями",
+    author: "Мартин Майер",
+    title: "Среди листьев",
+    description: "Цветная литография (40х60)",
+    price: "20 000 руб",
+  },
+  {
+    id: 4,
+    image: image10,
+    alt: "Желто-красная птица сидит на ветке",
+    author: "Герман Беккер",
+    title: "Яркая птица",
+    description: "Цветная литография (40х60)",
+    price: "13 000 руб",
+  },
+  {
+    id: 5,
+    image: image11,
+    alt: "Две красно-черных птицы сидят на стволе дерева",
+    author: "Вульф Бауэр",
+    title: "Дятлы",
+    description: "Бумага, акрил (50х80)",
+    price: "20 000 руб",
+  },
+  {
+    id: 6,
+    image: image12,
+    alt: "Окно с решеткой",
+    author: "Вальтер Хартманн",
+    title: "Большие воды",
+    description: "Бумага, акрил (50х80)",
+    price: "23 000 руб",
+  },
+];
+
+export const englandCards = [
+  {
+    id: 1,
+    image: image13,
+    alt: "Коричневый лось на белом фоне",
+    author: "Пол Смит",
+    title: "Дикий зверь",
+    description: "Акварель, бумага (50х80)",
+    price: "19 500 руб",
+  },
+  {
+    id: 2,
+    image: image14,
+    alt: "Рыжий кот на белом фоне сидит на земле",
+    author: "Альфред Барр",
+    title: "Рыжий кот",
+    description: "Цветная литография (40х60)",
+    price: "21 000 руб",
+  },
+  {
+    id: 3,
+    image: image15,
+    alt: "Скалистый берег реки и гора на заднем фоне",
+    author: "Джон Уайт",
+    title: "Скалистый берег",
+    description: "Цветная литография (40х60)",
+    price: "17 500 руб",
+  },
+  {
+    id: 4,
+    image: image16,
+    alt: "Зеленые берега реки и река",
+    author: "Джим Уотсон",
+    title: "Река и горы",
+    description: "Акварель, бумага (50х80)",
+    price: "20 500 руб",
+  },
+  {
+    id: 5,
+    image: image17,
+    alt: "Белый попугай с желтой холкой сидит на ветка",
+    author: "Юджин Зиллион",
+    title: "Белый попугай",
+    description: "Цветная литография (40х60)",
+    price: "15 500 руб",
+  },
+  {
+    id: 6,
+    image: image18,
+    alt: "Разноцветная рыба на черном фоне",
+    author: "Эрик Гиллман",
+    title: "Ночная рыба",
+    description: "Бумага, акрил (50х80)",
+    price: "12 500 руб",
   },
 ];
