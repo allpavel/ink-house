acbe44a45385f92a1fb65c951c6e8f8d407acfec
allpavel <platprincipal.adv@gmail.com>
2023-11-17 12:03:39 +0300

    add aria attributes to the cards

--- app/components/ui/Card/Card.tsx
+++ app/components/ui/Card/Card.tsx
@@ -1,4 +1,5 @@
 import Image, { StaticImageData } from "next/image";
+import { useId } from "react";
 
 interface CardProps {
   image: StaticImageData;
@@ -17,15 +18,32 @@ export default function Card({
   price,
   author,
 }: CardProps) {
+  const authorId = useId();
+  const titleId = useId();
+  const priceId = useId();
+  const descriptionId = useId();
+
   return (
-    <article className="bg-main-color-200 p-5 font-medium">
+    <article
+      className="bg-main-color-200 p-5 font-medium"
+      aria-label={`${authorId} ${titleId}`}
+      aria-describedby={`${descriptionId} ${priceId}`}
+    >
       <figure>
         <Image src={image} alt={alt} className="h-64 w-64" />
-        <figcaption className="py-3 text-black-200">{author}</figcaption>
+        <figcaption className="py-3 text-black-200" id={authorId}>
+          {author}
+        </figcaption>
       </figure>
-      <h3 className="pb-3 text-2xl">{title}</h3>
-      <p className="pb-7">{description}</p>
-      <p className="pb-3 text-main-color-400">{price}</p>
+      <h3 className="pb-3 text-2xl" id={titleId}>
+        {title}
+      </h3>
+      <p className="pb-7" id={descriptionId}>
+        {description}
+      </p>
+      <p className="pb-3 text-main-color-400" id={priceId}>
+        {price}
+      </p>
       <button className="w-full border-2 border-main-color-400 bg-transparent px-14 py-4 text-main-color-400 transition-colors hover:bg-main-color-400 hover:text-white">
         Купить
       </button>
